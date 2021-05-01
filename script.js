ddcf8f77fa8982f6578ebe2a9a333d1cbf419e83
Alison Moura <alison.moura@hightechcursos.com.br>
2021-05-01 12:16:16 -0400

    Commit aula 11 - Iniciando projeto lista de convidados

---
+++ script.js
@@ -0,0 +1,6 @@
+let convidados = ["Jão da Silva", "Maria da Silva", "Pafúncio Beltrano", "Edicrelson Ciclano"]
+
+function carregarLista() {
+    let div = document.getElementById("lista")
+    div.innerHTML = convidados
+}
